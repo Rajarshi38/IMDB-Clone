e1c0cc2b079607287da2e646b794bdaa1bf42586
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-03-01 11:55:15 +0530

    implemented dynamic nature

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,52 +1,32 @@
 import { getMovies } from "../api/Movies";
 import { useState, useEffect } from "react";
-import { Oval } from "react-loader-spinner";
-import axios from "axios";
-const decodeString = (str) => {
-    const textArea = document.createElement("textarea");
-    textArea.innerHTML = str;
-    return textArea.value;
-};
+import { TailSpin } from "react-loader-spinner";
 
 const Trending = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-        // getMovies().then((data) => {
-        //     setLoading(true);
-        //     setMovies(
-        //         data.results.map((res) => {
-        //             return {
-        //                 id: res.id,
-        //                 title: res.title ? res.title : res.name,
-        //                 rating: res.vote_average,
-        //                 media_type: res.media_type,
-        //             };
-        //         })
-        //     );
-        // });
-        // setLoading(false);
-        axios
-            .get(
-                `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
-            )
-            .then((res) => {
-                setLoading(true);
-                setMovies(
-                    res.data.results.map((res) => {
-                        return {
-                            id: res.id,
-                            title: res.title ? res.title : res.name,
-                            rating: res.vote_average,
-                            media_type: res.media_type,
-                        };
-                    })
-                );
-            });
-        setLoading(false);
+        const fetchData = async () => {
+            setLoading(true);
+            const response = await getMovies();
+
+            setMovies(
+                response.results.map((res) => {
+                    return {
+                        id: res.id,
+                        title: res.title ? res.title : res.name,
+                        rating: res.vote_average,
+                        media_type: res.media_type,
+                    };
+                })
+            );
+
+            setLoading(false);
+        };
+
+        fetchData();
     }, []);
 
-    if (!loading) console.table(movies);
     return (
         <div className="trending mt-4 mb-8">
             <h2 className="font-body text-2xl font-medium mb-6 text-gray-800 text-center">
@@ -54,36 +34,26 @@ const Trending = () => {
             </h2>
             {loading ? (
                 <div className="flex justify-center">
-                    <Oval
+                    <TailSpin
                         className="items-center"
                         height="100"
                         width="100"
-                        color="grey"
+                        color="#00BFFF"
                         ariaLabel="loading"
                     />
                 </div>
             ) : (
                 <div className="movies-list flex flex-wrap gap-8 justify-center">
-                    <div className="bg-hero-img h-[25vh] w-[140px] md:h-[35vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end hover:scale-110 ease-out duration-300">
-                        <div className="font-body text-white text-base bg-gray-900 bg-opacity-50 w-full p-2 flex justify-center rounded-b-xl">
-                            Title
+                    {movies.map((movie) => (
+                        <div
+                            className="bg-hero-img h-[25vh] w-[140px] md:h-[35vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end hover:scale-110 ease-out duration-300"
+                            key={movie.id}
+                        >
+                            <div className="font-body text-white text-sm bg-gray-900 bg-opacity-50 w-full p-2 flex justify-center text-center rounded-b-xl">
+                                {movie.title}
+                            </div>
                         </div>
-                    </div>
-                    <div className="bg-hero-img h-[25vh] w-[140px] md:h-[35vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end hover:scale-110 ease-out duration-300">
-                        <div className="font-body text-white text-base bg-gray-900 bg-opacity-50 w-full p-2 flex justify-center rounded-b-xl">
-                            Title
-                        </div>
-                    </div>
-                    <div className="bg-hero-img h-[25vh] w-[140px] md:h-[35vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end hover:scale-110 ease-out duration-300">
-                        <div className="font-body text-white text-base bg-gray-900 bg-opacity-50 w-full p-2 flex justify-center rounded-b-xl">
-                            Title
-                        </div>
-                    </div>
-                    <div className="bg-hero-img h-[25vh] w-[140px] md:h-[35vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end hover:scale-110 ease-out duration-300">
-                        <div className="font-body text-white text-base bg-gray-900 bg-opacity-50 w-full p-2 flex justify-center rounded-b-xl">
-                            Title
-                        </div>
-                    </div>
+                    ))}
                 </div>
             )}
         </div>
