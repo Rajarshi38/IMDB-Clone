0ba9e548fd0e628961e1660cecbf83d4552a945c
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-03-11 15:12:26 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Favourites from "./components/Favourites";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import Favourites from "./components/Favourites/Favourites";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Utils/Navbar";
 
 function App() {
     return (
